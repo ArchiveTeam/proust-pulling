9992ff9e4c1cb02196737a1c69d63a4e60c4314d
David Yip <yipdw@member.fsf.org>
2012-01-05 09:13:50 -0600

    Limit scope to a single UID.

--- get_story.rb
+++ get_story.rb
@@ -6,18 +6,9 @@ require 'fileutils'
 include FileUtils
 
 VERSION     = `git log -n1 --oneline #{$0} | awk '{print $1}'`.chomp
-WGET_WARC   = './wget-warc'
+WGET_WARC   = File.expand_path('../wget-warc', __FILE__)
 USER_AGENT  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.54 Safari/535.2'
 DOWNLOAD_TO = File.expand_path('../data', __FILE__)
-
-EXCLUDED_DIRECTORIES = %w(
-  questions
-  world-events
-  people
-  tour
-  about
-).join(',')
-
 def usage
   puts "#{$0} [user ID]"
 end
@@ -28,26 +19,43 @@ unless ARGV[0]
 end
 
 uid = URI.encode(ARGV[0])
-warc_file = "#{DOWNLOAD_TO}/#{uid[0..0]}/#{uid[0..1]}/#{uid[0..2]}/#{uid}"
+warc_file = "#{DOWNLOAD_TO}/#{uid[0..0]}/#{uid[0..1]}/#{uid[0..2]}/#{uid}/#{uid}"
 mkdir_p File.dirname(warc_file)
+
+BASE_URL = lambda { |rest| "http://www.proust.com/story/#{uid}/#{rest}" }
+ACCEPT = %W(
+  story/#{uid}
+  pi*
+  img*
+  js*
+  css*
+).join(',')
 
 cmd = [
   WGET_WARC,
-  "-U #{USER_AGENT}",
+  "-U '#{USER_AGENT}'",
+  "-o #{warc_file}.log",
+  "-e 'robots=off'",
   "--warc-file #{warc_file}",
   "--warc-max-size=inf",
   "--warc-cdx",
   "--warc-header='operator: Archive Team'",
   "--warc-header='proust-dld-script-version: #{VERSION}'",
   "-nv",
+  "-np",
   "-r",
   "-l inf",
   "--no-remove-listing",
   "--no-timestamping",
   "--page-requisites",
   "--trust-server-names",
-  "--exclude-directories=#{EXCLUDED_DIRECTORIES}",
-  "http://www.proust.com/story/#{uid}"
+  "--force-directories",
+  BASE_URL[''],
+  BASE_URL['all'],
+  BASE_URL['map'],
+  BASE_URL['timeline'],
+  BASE_URL['memorabilia'],
+  BASE_URL['tagged']
 ]
 
 puts cmd.join(' ')
