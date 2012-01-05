a39afb03fabad9367d9e4a6e9b54fc980f72ef65
David Yip <yipdw@member.fsf.org>
2012-01-05 11:05:42 -0600

    Record in-progress and pending members.
    This allows for multiple get_stories processes.

--- get_stories.rb
+++ get_stories.rb
@@ -3,18 +3,50 @@ require 'escape'
 
 r = Redis.new
 
-r.sdiffstore 'proust_todo', 'proust_users', 'proust_done'
+# All known users.
+KNOWN   = 'proust_users'
 
-r.smembers('proust_todo').each do |member|
-  puts "Retrieving #{member}."
+# Downloads in progress.
+WORKING = 'proust_working'
+
+# Downloads completed.
+DONE    = 'proust_done'
+
+# Known + working.
+PENDING = 'proust_pending'
+
+# Pending - done.
+TODO    = 'proust_todo'
+
+r.sunionstore PENDING, KNOWN, WORKING
+r.sdiffstore TODO, PENDING, DONE
+
+loop do
+  member = r.srandmember(PENDING).tap do |m|
+    if m
+      r.multi do
+        r.srem PENDING, m
+        r.sadd WORKING, m
+      end
+    else
+      puts "Nothing left to do."
+      exit 0
+    end
+  end
+
+  puts "Fetching data for #{member}."
+
   cmd = `./gen_story_cmd.rb #{Escape.shell_single_word(member)}`
-
   puts cmd
   `#{cmd}`
 
   if $? == 0
     puts "Retrieved #{member} successfully."
-    r.sadd 'proust_done', member
+
+    r.multi do
+      r.sadd DONE, member
+      r.srem WORKING, member
+    end
   else
     puts "Errors encountered retrieving #{member}."
   end
