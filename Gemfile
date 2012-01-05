da0bed5086a0b3f31b6b60be1c82c0396556db5a
David Yip <yipdw@member.fsf.org>
2012-01-05 10:28:48 -0600

    Escape shell arguments, rearrange code.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 gem 'mechanize'
 gem 'redis'
+gem 'escape'
--- get_story.rb
+++ get_story.rb
@@ -1,14 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'uri'
+require 'escape'
 require 'fileutils'
 
 include FileUtils
 
-VERSION     = `git log -n1 --oneline #{$0} | awk '{print $1}'`.chomp
-WGET_WARC   = File.expand_path('../wget-warc', __FILE__)
-USER_AGENT  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.54 Safari/535.2'
-DOWNLOAD_TO = File.expand_path('../data', __FILE__)
 def usage
   puts "#{$0} [user ID]"
 end
@@ -19,10 +16,7 @@ unless ARGV[0]
 end
 
 uid = URI.encode(ARGV[0])
-warc_file = "#{DOWNLOAD_TO}/#{uid[0..0]}/#{uid[0..1]}/#{uid[0..2]}/#{uid}/#{uid}"
-mkdir_p File.dirname(warc_file)
 
-BASE_URL = lambda { |rest| "http://www.proust.com/story/#{uid}/#{rest}" }
 ACCEPT = %W(
   story/#{uid}
   pi*
@@ -31,12 +25,22 @@ ACCEPT = %W(
   css*
 ).join(',')
 
+E           = lambda { |word| Escape.shell_single_word(word) }
+URL         = lambda { |rest| E["http://www.proust.com/story/#{uid}/#{rest}"] }
+VERSION     = `git log -n1 --oneline #{$0} | awk '{print $1}'`.chomp
+WGET_WARC   = File.expand_path('../wget-warc', __FILE__)
+USER_AGENT  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.54 Safari/535.2'
+DOWNLOAD_TO = File.expand_path('../data', __FILE__)
+
+warc_file = "#{DOWNLOAD_TO}/#{uid[0..0]}/#{uid[0..1]}/#{uid[0..2]}/#{uid}/#{uid}"
+mkdir_p File.dirname(warc_file)
+
 cmd = [
   WGET_WARC,
-  "-U '#{USER_AGENT}'",
-  "-o #{warc_file}.log",
+  "-U " + E[USER_AGENT],
+  "-o " + E["#{warc_file}.log"],
   "-e 'robots=off'",
-  "--warc-file #{warc_file}",
+  "--warc-file=" + E[warc_file],
   "--warc-max-size=inf",
   "--warc-cdx",
   "--warc-header='operator: Archive Team'",
@@ -50,12 +54,12 @@ cmd = [
   "--page-requisites",
   "--trust-server-names",
   "--force-directories",
-  BASE_URL[''],
-  BASE_URL['all'],
-  BASE_URL['map'],
-  BASE_URL['timeline'],
-  BASE_URL['memorabilia'],
-  BASE_URL['tagged']
+  URL[''],
+  URL['all'],
+  URL['map'],
+  URL['timeline'],
+  URL['memorabilia'],
+  URL['tagged']
 ]
 
 puts cmd.join(' ')
