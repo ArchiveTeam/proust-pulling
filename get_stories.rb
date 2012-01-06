5276aa1fcb66da4f656516702f35ce24d53592fe
David Yip <yipdw@member.fsf.org>
2012-01-06 03:20:26 -0600

    Use Redis key expiration for work tracking.

--- get_stories.rb
+++ get_stories.rb
@@ -12,21 +12,18 @@ WORKING = 'proust_working'
 # Downloads completed.
 DONE    = 'proust_done'
 
-# Known + working.
-PENDING = 'proust_pending'
-
 # Pending - done.
 TODO    = 'proust_todo'
 
-r.sunionstore PENDING, KNOWN, WORKING
-r.sdiffstore TODO, PENDING, DONE
+r.sdiffstore TODO, KNOWN, DONE
 
 loop do
-  member = r.srandmember(PENDING).tap do |m|
+  member = r.srandmember(TODO).tap do |m|
     if m
       r.multi do
-        r.srem PENDING, m
+        r.srem TODO, m
         r.sadd WORKING, m
+        r.setex "proust_#{m}_watch", (60 * 60 * 2), 1
       end
     else
       $stderr.puts "Nothing left to do."
@@ -44,6 +41,7 @@ loop do
     r.multi do
       r.sadd DONE, member
       r.srem WORKING, member
+      r.del "proust_#{m}_watch"
     end
   else
     $stderr.puts "Errors encountered retrieving #{member}."
